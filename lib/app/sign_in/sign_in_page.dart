cdd5fb4f336268ae60dc40c76f80c9f830a58e16
Dariusz Zamorski <dariusz.zamorski@gmail.com>
2021-07-06 20:25:57 +0200

    step 1

---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+        elevation: 2.0,
+      ),
+      body: Container(
+          color: Colors.yellow,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                color: Colors.orange,
+                child: SizedBox(
+                  height: 100.0,
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
